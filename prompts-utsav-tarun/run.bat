b98f0da4ad6880edf1e41800404155c3b70ae229
tarun-bisht625 <tarunbisht756@gmail.com>
2026-09-15 17:47:00 +0530

    Delete prompts-utsav-tarun directory

--- prompts-utsav-tarun/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title The Architect's Blueprint - Engineers' Day
-echo ========================================================
-echo   THE ARCHITECT'S BLUEPRINT - ENGINEERS' DAY
-echo ========================================================
-echo Starting local web server at http://localhost:8080 ...
-start "" "http://localhost:8080"
-py -m http.server 8080
-pause
